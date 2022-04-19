fc97eae1a142b5699f9e85f9725d905b3beb7b5b
zyc <870020194@qq.com>
2022-04-19 17:36:56 +0800

    页面布置

---
+++ dist/build/mp-weixin/pages/.js
@@ -0,0 +1,28 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/"],{
+
+/***/ 35:
+/*!*****************************************!*\
+  !*** ./src/main.js?{"page":"pages%2F"} ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {
+
+__webpack_require__(/*! uni-pages */ 5);
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+
+var _vue2 = _interopRequireDefault(__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module './pages/.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_vue2.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ })
+
+},[[35,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../.sourcemap/mp-weixin/pages/.js.map
